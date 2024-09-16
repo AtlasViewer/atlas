0265dd059f277c946e1a124d0ed7334304b969f0
zontreck <tarapiccari@gmail.com>
2024-09-16 12:40:19 -0700

    Alter jenkinsfile

---
+++ Assets/Scripts/Editor/BuildCommand.cs
@@ -0,0 +1,166 @@
+using UnityEditor;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+static class BuildCommand
+{
+    private const string IS_DEVELOPMENT_BUILD = "IS_DEVELOPMENT_BUILD";
+
+    public static void ProcessCLIBuild() {
+        Console.WriteLine("> PERFORM BUILD");
+
+        var buildTarget = GetBuildTarget();
+        var targetGroup = GetBuildTargetGroup(buildTarget);
+
+        var buildPath = GetBuildPath();
+        var buildName = GetBuildName();
+        var buildOpts = GetBuildOptions();
+        var fixedPath = GetFixedBuildPath(buildTarget, buildPath, buildName);
+
+        var buildReport = BuildPipeline.BuildPlayer(GetEnabledScenes(), fixedPath, buildTarget, buildOpts);
+
+        if(buildReport.summary.result != UnityEditor.Build.Reporting.BuildResult.Succeeded)
+            throw new Exception($"Build ended with {buildReport.summary.result} status");
+
+        Console.WriteLine("> BUILD DONE");
+    }
+
+    static bool IsDevelopmentType() {
+        if(TryGetEnv(IS_DEVELOPMENT_BUILD, out string val)) {
+            return bool.parse(value);
+        }
+
+        Console.WriteLine("> NO DEV_ENV FLAG FOUND, DEFAULTING TO NO");
+        return false;
+    }
+
+    private static void HandleDevelopmentBuildType(bool isDevBuild) {
+        EditorUserBuildSettings.development = isDevBuild;
+        PlayerSettings.SplashScreen.show = isDevBuild;
+    }
+
+    static BuildTarget GetBuildTarget() {
+        string buildTgtName = GetArgument("buildTarget");
+
+        if(buildTargetName.TryConvertToEnum(out BuildTarget tgt))
+            return tgt;
+
+        return BuildTarget.NoTarget;
+    }
+
+    static BuildTargetGroup(BuildTarget tgt) {
+        string tgtGroup;
+        string platform = tgt.ToString();
+
+        if(tgt.ToString().ToLower().Contains("standalone"))
+            tgtGroup="Standalone";
+        else if(tgt.ToString().ToLower().Contains("wsa"))
+            tgtGroup = "WSA";
+        else tgtGroup = platform;
+
+        if (tgtGroup.TryConvertToEnum(out BuildTargetGroup tgtGrp))
+            return tgtGrp;
+
+        return BuildTargetGroup.Unknown;
+    }
+
+    static bool TryGetEnv(string key, out string val) {
+        val = Environment.GetEnvironmentVariable(key);
+        return !string.IsNullOrEmpty(val);
+    }
+
+    static string GetArgument(string name) {
+        string[] args = Environment.GetCommandLineArgs();
+        for(int i=0;i<args.Length; i++) {
+            if(args[i].Contains(name)) return args[i+1];
+        }
+
+        return null;
+    }
+
+
+    public static bool TryConvertToEnum<TEnum>(this string strEnumValue, out TEnum value)
+    {
+        if (!Enum.IsDefined(typeof(TEnum), strEnumValue))
+        {
+            value = default;
+            return false;
+        }
+
+        value = (TEnum)Enum.Parse(typeof(TEnum), strEnumValue);
+        return true;
+    }
+
+    static string GetBuildPath()
+    {
+        string buildPath = GetArgument("customBuildPath");
+        Console.WriteLine(":: Received customBuildPath " + buildPath);
+        if (buildPath == "")
+        {
+            throw new Exception("customBuildPath argument is missing");
+        }
+        return buildPath;
+    }
+
+    static string GetBuildName()
+    {
+        string buildName = GetArgument("customBuildName");
+        Console.WriteLine(":: Received customBuildName " + buildName);
+        if (buildName == "")
+        {
+            throw new Exception("customBuildName argument is missing");
+        }
+        return buildName;
+    }
+
+    static string GetFixedBuildPath(BuildTarget buildTarget, string buildPath, string buildName)
+    {
+        if (buildTarget.ToString().ToLower().Contains("windows")) {
+            buildName += ".exe";
+        } else if (buildTarget == BuildTarget.Android) {
+            buildName += EditorUserBuildSettings.buildAppBundle ? ".aab" : ".apk";
+        }
+        return buildPath + buildName;
+    }
+
+    static BuildOptions GetBuildOptions()
+    {
+        if (TryGetEnv(BUILD_OPTIONS_ENV_VAR, out string envVar)) {
+            string[] allOptionVars = envVar.Split(',');
+            BuildOptions allOptions = BuildOptions.None;
+            BuildOptions option;
+            string optionVar;
+            int length = allOptionVars.Length;
+
+            Console.WriteLine($":: Detecting {BUILD_OPTIONS_ENV_VAR} env var with {length} elements ({envVar})");
+
+            for (int i = 0; i < length; i++) {
+                optionVar = allOptionVars[i];
+
+                if (optionVar.TryConvertToEnum(out option)) {
+                    allOptions |= option;
+                }
+                else {
+                    Console.WriteLine($":: Cannot convert {optionVar} to {nameof(BuildOptions)} enum, skipping it.");
+                }
+            }
+
+            return allOptions;
+        }
+
+        return BuildOptions.None;
+    }
+    static string[] GetEnabledScenes()
+    {
+        return (
+            from scene in EditorBuildSettings.scenes
+            where scene.enabled
+            where !string.IsNullOrEmpty(scene.path)
+            select scene.path
+        ).ToArray();
+    }
+
+
+}
--- Jenkinsfile
+++ Jenkinsfile
@@ -6,6 +6,11 @@ pipeline {
         buildDiscarder(logRotator(numToKeepStr: '5'))
     }
 
+    environment {
+        IS_DEVELOPMENT_BUILD = true // Temporarily, all builds are dev builds. In the future only the dev branch will be development builds
+        // A second flag will be added on first initial release: SNAPSHOT. Which will indicate if the viewer is a snapshot of the live branch or a build issued by the team.
+    }
+
     stages {
         stage("Build on Windows") {
             agent {
@@ -17,7 +22,7 @@ pipeline {
                     bat '''
                     @echo off
 
-                    Unity -quit -projectPath "%cd%" -batchmode -buildTarget win64
+                    Unity -quit -projectPath "%cd%" -batchmode -buildTarget win64 -customBuildPath "%cd%\\out\\" -customBuildName "AtlasViewer" -executeMethod BuildCommand.ProcessCLIBuild
                     '''
                 }
             }
